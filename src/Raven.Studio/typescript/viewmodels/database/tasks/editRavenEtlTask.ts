020eb1834f9c3c8b1fae9e8ca0199c0b4dc0755e
Maksym Smoliński <maksym.smolinski@ravendb.net>
2026-03-17 15:21:46 +0100

    RavenDB-21192 fix (ETL tasks): fix ts errors

--- src/Raven.Studio/typescript/viewmodels/database/tasks/editRavenEtlTask.ts
+++ src/Raven.Studio/typescript/viewmodels/database/tasks/editRavenEtlTask.ts
@@ -57,7 +57,7 @@ class ravenTaskTestMode {
     debugOutput = ko.observableArray<string>([]);
 
     // all kinds of alerts:
-    transformationErrors = ko.observableArray<Raven.Server.NotificationCenter.Notifications.Details.EtlErrorInfo>([]);
+    transformationErrors = ko.observableArray<Raven.Server.Documents.ETL.EtlItemError>([]);
 
     warningsCount = ko.pureComputed(() => {
         return this.transformationErrors().length;
@@ -156,7 +156,7 @@ class ravenTaskTestMode {
                         };
                     }));
                     this.debugOutput(simulationResult.DebugOutput);
-                    this.transformationErrors(simulationResult.TransformationErrors);
+                    this.transformationErrors(simulationResult.ItemTransformationErrors);
 
                     if (this.warningsCount()) {
                         $('.test-container a[href="#warnings"]').tab('show');
